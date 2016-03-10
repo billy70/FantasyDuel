e2a55b0e6b50b00718c5ecb0d0f99e8aeb451222
William L. Marr III <hobbyist@wlmiii.net>
2016-03-10 14:17:04 -0500

    Add source file for Potion enumeration

---
+++ FantasyDuel/Potion.swift
@@ -0,0 +1,15 @@
+//
+//  Potion.swift
+//  FantasyDuel
+//
+//  Created by William L. Marr III on 3/10/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import Foundation
+
+enum PotionType {
+    case Health
+    case Armor
+    case Attack
+}
