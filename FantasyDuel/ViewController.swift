e9c56a3d5f9fd89d73a6deb2a2099ac5cef0bd49
William L. Marr III <hobbyist@wlmiii.net>
2016-03-10 12:13:57 -0500

    Add outlets and actions, and set player name and potions to hidden

--- FantasyDuel/ViewController.swift
+++ FantasyDuel/ViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var statusText: UILabel!
+    @IBOutlet weak var playerNameLabel: UITextField!
+    @IBOutlet weak var potionStackView: UIStackView!
+    @IBOutlet weak var playerOneButton: UIButton!
+    @IBOutlet weak var playerTwoButton: UIButton!
+    
+
+    // MARK: - Overrides
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +33,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: - Action methods
+    
+    @IBAction func playerOneButtonTapped(sender: AnyObject) {
+    }
 
+    @IBAction func playerTwoButtonTapped(sender: AnyObject) {
+    }
+
+    @IBAction func healthPotionTapped(sender: AnyObject) {
+    }
+    
+    @IBAction func armorPotionTapped(sender: AnyObject) {
+    }
+    
+    @IBAction func attackPotionTapped(sender: AnyObject) {
+    }
 }
 
